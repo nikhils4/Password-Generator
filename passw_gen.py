dd8d5742534c4eec9295635dd9b90070ceee33c7
Nikhil Singh <30321610+ImSnik@users.noreply.github.com>
2018-09-17 02:39:39 +0530

    Google Drive upload feature added

--- passw_gen.py
+++ passw_gen.py
@@ -7,11 +7,47 @@ import random
 import os
 import os.path
 import pyperclip
-print("\t\t\t\tWelcome to paswword generator\n\n")
+
+import time
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+    
+print("\t\t\t\tWelcome to paswword generator\n")
+
+
+#adding drive upload functionality
+def drive():
+    drive_q = str(input("Do you want to save the password to your Google Drive : "))
+
+    if os.path.isfile("secured_pw.txt") == True:
+        if drive_q in ['Y','y', 'yes','YES', 'Yes']:
+            gauth = GoogleAuth()  # Authentication
+            gauth.LocalWebserverAuth()
+            drive = GoogleDrive(gauth)
+            
+            # file upload procedure
+            file_n = str(input("Enter the filename to be save in Google Drive : "))
+            file1 = drive.CreateFile({'title': file_n + ' ' +  time.ctime()})
+            file1.SetContentFile('secured_pw.txt')
+            file1.Upload()
+            print("\nUploading......\n")
+            print("File sucessfully uploaded !")
+            print("Closing.....")
+            time.sleep(7)
+            exit()
+        else:
+            menu()
+    else:
+        print("File to be uploaded does not exist !")
+        menu()
+
+        
+        
 #adding functionality of menu based operations
 def menu():
-    print("\n\n\t\t\t\tMENU\t\t\t\t")
-    print("\t\t\t\t1. Generate password\n\t\t\t\t2. Delete password file\n\t\t\t\t3. Search for password\n\t\t\t\t4. Display saved p/w name\n\t\t\t\t5. Exit")
+    print("\n\t\t\t\tMENU\t\t\t\t")
+    print("\t\t\t\t1. Generate password\n\t\t\t\t2. Delete password file\n\t\t\t\t3. Search for password\n\t\t\t\t4. Display saved p/w name\n\t\t\t\t5. Save to Drive\n\t\t\t\t6. Exit")
     
     option = int(input("Enter the number for operation : "))
     
@@ -24,6 +60,8 @@ def menu():
     if option == 4:
         disp_pw_name()
     if option == 5:
+        drive()
+    if option == 6:
         print("Thanks for using Snapnab_N1 Password Generator")
         exit()
     
